e49237e6900f685c0d9569ed78f69831c4fec9ea
leQuimera <lequimera@yandex.ru>
2017-03-30 16:38:26 +0300

    Корректировка файла скрипта

--- js/stat.js
+++ js/stat.js
@@ -1,14 +1,13 @@
 'use strict';
 
-
 var drawBox = function (ctx, cloudX, cloudY, cloudColor) {
   ctx.fillStyle = cloudColor;
   ctx.fillRect(cloudX, cloudY, 420, 270);
-}
+};
 
 function getMaxOfArray(numArray) {
   return Math.max.apply(null, numArray);
-}
+};
 
 var renderHisto = function (ctx, names, times) {
   var max = getMaxOfArray(times);
@@ -16,16 +15,16 @@ var renderHisto = function (ctx, names, times) {
   var step = histogramHeight / max;
   max = (max / 1000).toFixed(2);
 
-  for ( var i = 0 ; i < names.length ; i++) {
+  for (var i = 0; i < names.length; i++) {
     var countY = 250 - times[i] * step;
     var contHeight = 250 - countY;
-    var histColor = (names[i] == 'Вы') ? 'rgba(255, 0, 0, 1)' : '#303030';
+    var histColor = (names[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : '#303030';
     var countX = 140 + 90 * i;
     ctx.fillStyle = histColor;
     ctx.fillRect(countX, countY, 40, contHeight);
     ctx.strokeText(names[i], countX, 270);
   }
-}
+};
 
 window.renderStatistics = function (ctx, names, times) {
   drawBox(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
