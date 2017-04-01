f6211411f58e524c55133965cb100b39430fcfb6
Le Quimera <jffarh@gmail.com>
2017-04-01 18:00:29 +0300

    Corriges

--- js/stat.js
+++ js/stat.js
@@ -5,7 +5,10 @@ var cloudTopX = 100;
 var cloudTopY = 10;
 var cloudHeght = 270;
 var cloudWidth = 420;
+var histogramHeight = 150;
 var histWidth = 40;
+var indent = 50;
+var colonStep = indent + histWidth;
 var colorBase = '#fff';
 var colorShadow = 'rgba(0, 0, 0, 0.7)';
 
@@ -20,27 +23,25 @@ function getMaxOfArray(numArray) {
   return Math.max.apply(null, numArray);
 }
 
-var choseColor = function (gamerName) {
-  if (gamerName === 'Вы') {
-    var histColor = 'rgba(255, 0, 0, 1)';
-  } else {
-    histColor = 'rgba(11, 18, 110, ' + Math.random() + ')';
-  }
-  return histColor;
+// Вычисление цвета для колонки
+var getColor = function () {
+  var tmpHistColor = 'rgba(11, 18, 110, ' + Math.random() + ')';
+  return tmpHistColor;
 };
 
 // Построение гистограммы (канвас, имена игроков, результаты)
 var renderHisto = function (ctx, names, times) {
   var max = getMaxOfArray(times);
-  var histogramHeight = 150;
   var step = histogramHeight / max;
   max = (max / 1000).toFixed(2);
 
   for (var i = 0; i < names.length; i++) {
     var positionY = botomY - times[i] * step;
     var positionHeight = botomY - positionY;
-    var histColor = choseColor(names[i]);
-    var positionX = 140 + 90 * i;
+    var histColor = (names[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : getColor();
+    var positionX = 140 + colonStep * i;
+    var timeSet = Math.round(times[i]);
+    ctx.strokeText(timeSet, positionX, positionY - 10);
     ctx.fillStyle = histColor;
     ctx.fillRect(positionX, positionY, histWidth, positionHeight);
     ctx.strokeText(names[i], positionX, cloudHeght);
